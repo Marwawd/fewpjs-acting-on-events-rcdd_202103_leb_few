12c81299dd9326f3e8fb30085e4cbf3266d9a423
Marwa  Awwad <marwaawwad10@gmail.com>
2021-05-08 15:40:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,14 @@
 //   if (e.key === "ArrowLeft") {
 //     let leftNumbers = dodger.style.left.replace("px", "");
 //     let left = parseInt(leftNumbers, 10);
-// 
+//
 //     dodger.style.left = `${left - 1}px`;
 //   }
 // });*/
 // function moveDodgerLeft() {
 //   let leftNumbers = dodger.style.left.replace("px", "");
 //   let left = parseInt(leftNumbers, 10);
-// 
+//
 //   if (left > 0) {
 //     dodger.style.left = `${left - 1}px`;
 //   }
